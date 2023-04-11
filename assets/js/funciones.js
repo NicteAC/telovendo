58091f76e32fff3e99c9ebbd76daff2c9e75f1a5
NicteAC <carola.aliaga@gmail.com>
2023-04-10 21:32:25 -0400

    add js y dataTable

--- assets/js/funciones.js
+++ assets/js/funciones.js
@@ -1,10 +1,29 @@
 const button = document.getElementById("button-form");
-const form = document.getElementById("form");
 
 const inputName = document.getElementById("exampleInputName1");
+const inputEmail = document.getElementById("exampleInputEmail1");
+const inputMessage = document.getElementById("floatingTextarea");
 
-function validateForm() {}
+const inputNameText = document.getElementById("inputName");
+const inputEmailText = document.getElementById("inputEmail");
+const inputMessageText = document.getElementById("inputText");
 
-form.addEventListener("submit", function (event) {
-  event.preventDefault();
-});
+function validateForm() {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    if (inputName.value == "") {
+      inputNameText.innerHTML = '* Ingresar campo obligatorio'
+    }
+    if (inputEmail.value == "") {
+      inputEmailText.innerHTML = '* Ingresar campo obligatorio'
+    }
+    if (inputMessage.value == "") {
+      inputMessageText.innerHTML = '* Ingresar campo obligatorio'
+    }
+  });
+}
+
+$(document).ready( function(){
+  $('#myTable').DataTable()
+})
